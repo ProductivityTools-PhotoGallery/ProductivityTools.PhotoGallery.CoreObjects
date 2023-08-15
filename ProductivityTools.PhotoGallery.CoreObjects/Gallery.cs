d48d82035c857e13666255f14825035c6cd3ce4c
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-08-15 22:02:44 +0200

    core objects

---
+++ ProductivityTools.PhotoGallery.CoreObjects/Gallery.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductivityTools.PhotoGallery.CoreObjects
+{
+    public class Gallery
+    {
+        public Gallery()
+        {
+            this.ImageList = new List<Image>();
+            this.ImageSizes = new List<int>();
+        }
+        public List<string> ReadGmails { get; set; }
+
+        public List<Image> ImageList { get; set; }
+        public List<int> ImageSizes { get; set; }
+    }
+}
